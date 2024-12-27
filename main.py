fb431188262c01a131ba84768b7e4194f357af11
alwaysLernin <curtis.coughlin@gmail.com>
2024-12-27 14:46:53 -0700

    Completed project

--- main.py
+++ main.py
@@ -1,19 +1,39 @@
+all_lower = "abcdefghijklmnopqrstuvwxyz"
+lowercase_dict = {key: 0 for key in all_lower}
+
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
-    count_words(text)
+    word_count = count_words(text)
+    char_count = get_char_count(text)
     #print(text)
-    print(count_words(text))
+    #print(count_words(text))
+    #print(char_count)
+    
+    print(f"--- Begin report of {book_path}")
+    print(f"{word_count} words found in the document")
+    for key, value in char_count.items():
+        print(f"The '{key}' character was found '{value}' times")
+
+
 
 def get_book_text(path):
     with open(path) as f:
         return f.read()
     
 def count_words(text):
-    word_count = 0
     words = text.split(" ")
-    #word_count += len(words)
     return len(words)
+
+def get_char_count(text):
+    for l in text:
+        lowered_string = l.lower()
+        if lowered_string in lowercase_dict:
+            lowercase_dict[lowered_string] += 1
+    
+    return lowercase_dict
+
+    
 
 main()
     
